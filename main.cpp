0fa105ecbac557c764a1e5ee2fdf94f02d00abe5
Jaaackoo <jack.hascoet@gmail.com>
2025-10-14 13:19:58 +0200

    push pour annabelle

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 #include "Vehicule.hpp"
 
 void test_vehicule(void);
+void test_vehiculeConstructeur(void);
+void test_vehiculeDemarrer(void);
+void test_vehiculeArreter(void);
+void test_vehiculeDepanner(void);
+void test_vehiculeAccelerer(void);
+void test_vehiculeMonter(void);
+void test_vehiculeDescendre(void);
 void test_vehiculeMettrePanne(void);
+void test_vehiculeGetEtat(void);
 
 int main()
 {
@@ -17,16 +25,37 @@ int main()
 void test_vehicule(void)
 {
 
-    test_vehiculeMettrePanne();
+    test_vehiculeConstructeur();
+    test_vehiculeDemarrer();
+    test_vehiculeArreter();
+    test_vehiculeDepanner();
+    test_vehiculeAccelerer();
+    test_vehiculeMonter();
+    test_vehiculeDescendre();
+    // test_vehiculeMettrePanne();
+    test_vehiculeGetEtat();
 
     std::cout << "Class Vehicule: OK" << std::endl;
 }
 
-void test_vehiculeMettreEnPanne(void)
-{
+void test_vehiculeConstructeur(void) {
+    
+    // Constructeur avec les valeurs par défauts
     Vehicule v1 = Vehicule();
+    // Constructeur avec des valeurs qui fonctionent
+    Vehicule v2 = Vehicule(1, 2, 2);
 
-    // Ensure we do not go below double 0.0
+        
+
+}
+
+void test_vehiculeMettrePanne(void)
+{
+
+    Vehicule v1 = Vehicule();
+    Vehicule v2 = Vehicule();
+
+    // Test avec une valeur inférieur à 0
     try
     {
         v1.mettreEnPanne(-0.1);
@@ -37,7 +66,7 @@ void test_vehiculeMettreEnPanne(void)
     }
     assert(v1.getEtat() == "ARRET");
 
-    // Ensure we do not exceed double 1.0
+    // Test avec une valeur supérieur à 1
     try
     {
         v1.mettreEnPanne(1.1);
@@ -48,5 +77,30 @@ void test_vehiculeMettreEnPanne(void)
     }
     assert(v1.getEtat() == "ARRET");
 
+    v1.mettreEnPanne(0.2);
+    assert(v1.getEtat() == "PANNE_LEGERE");
 
+    v2.mettreEnPanne(0.6);
+    assert(v2.getEtat() == "PANNE_SEVERE");
+
+    // Test sur des véhicules déjà en panne
+    try
+    {
+        v1.mettreEnPanne(0.6);
+    }
+    catch (std::runtime_error const &ex)
+    {
+        std::cout << "Catched error: " << ex.what() << std::endl;
+    }
+    assert(v1.getEtat() == "PANNE_LEGERE");
+
+    try
+    {
+        v2.mettreEnPanne(0.4);
+    }
+    catch (std::runtime_error const &ex)
+    {
+        std::cout << "Catched error: " << ex.what() << std::endl;
+    }
+    assert(v2.getEtat() == "PANNE_SEVERE");
 }
--- src/Vehicule.cpp
+++ src/Vehicule.cpp
@@ -1,106 +1,118 @@
 #include "Vehicule.hpp"
 
-Vehicule::Vehicule(int vitesseMax, int nbPlaces, int occupants){
+Vehicule::Vehicule(int vitesseMax, int nbPlaces, int occupants)
+{
     // La vitesse ne depasse pas la limite max
-    if (vitesseMax < 0){
-        throw new invalid_argument("Le véhicule doit avoir une vitesse max positive ");
+    if (vitesseMax < 0)
+    {
+        throw invalid_argument("Le véhicule doit avoir une vitesse max positive ");
     }
 
     // Le vehicule possède au moins une place
-    if (nbPlaces <= 0){
-        throw new invalid_argument("Le véhicule doit avoir au moins une place");
+    if (nbPlaces <= 0)
+    {
+        throw invalid_argument("Le véhicule doit avoir au moins une place");
     }
 
     // occupants doit être positif
-    if (occupants < 0){
-        throw new invalid_argument("Le nombre d'occupant ne peut pas être négatif");
+    if (occupants < 0)
+    {
+        throw invalid_argument("Le nombre d'occupant ne peut pas être négatif");
     }
 
     // nbPlace ne peut pas etre inf à occupants
-    if (occupants > nbPlaces){
-        throw new invalid_argument("Les occupants doivent avoir une place");
+    if (occupants > nbPlaces)
+    {
+        throw invalid_argument("Les occupants doivent avoir une place");
     }
 
     this->vitesseMax_ = vitesseMax;
     this->nbPlaces_ = nbPlaces;
-    this -> occupants_ = occupants;
+    this->occupants_ = occupants;
 
     this->etat_ = this->ARRET;
     this->vitesse_ = 0;
 }
 
-void Vehicule::demarrer() 
+void Vehicule::demarrer()
 {
     // au moins 1 occupant
-    if (this->occupants_ <= 0 ) {
-        throw new invalid_argument("Le véhicule doit avoir au moins un occupant.");
+    if (this->occupants_ <= 0)
+    {
+        throw runtime_error("Le véhicule doit avoir au moins un occupant.");
     }
 
-    // si panne demarage impossible 
-    if(this->etat_ != this->ARRET) 
+    // si panne demarage impossible
+    if (this->etat_ != this->ARRET)
     {
-        throw new invalid_argument("Le véhicule est dans un mauvais état: " + this->getEtat());        
+        throw runtime_error("Le véhicule est dans un mauvais état: " + this->getEtat());
     }
-    
-    // Etat vehicule => Marche     
+
+    // Etat vehicule => Marche
     this->etat_ = this->MARCHE;
 
     // initialiser la vitesse à 0
     this->vitesse_ = 0;
-    
 }
 
-
-void Vehicule::arreter(){
+void Vehicule::arreter()
+{
 
     // Quelqu'un dans la caisse
-    if (this->occupants_ <= 0 ) {
-        throw new invalid_argument("Le véhicule doit avoir au moins un occupant.");
+    if (this->occupants_ <= 0)
+    {
+        throw runtime_error("Le véhicule doit avoir au moins un occupant.");
     }
 
     // Etat du vehicule autre que en marche
-    if(this->etat_ != this->MARCHE) 
+    if (this->etat_ != this->MARCHE)
     {
-        throw new invalid_argument("Le véhicule est dans un mauvais état: " + this->getEtat());        
+        throw runtime_error("Le véhicule est dans un mauvais état: " + this->getEtat());
     }
 
-    // Etat vehicule => ARRET     
+    // Etat vehicule => ARRET
     this->etat_ = this->ARRET;
 
-    // mettre la vitesse à 0 
+    // mettre la vitesse à 0
     this->vitesse_ = 0;
 }
 
-
-void Vehicule::depanner(){
+void Vehicule::depanner()
+{
 
     // Etat du vehicule anormal
-    if(this->etat_ == this->MARCHE || this->etat_ == this->ARRET) 
+    if (this->etat_ == this->MARCHE || this->etat_ == this->ARRET)
     {
-        throw new invalid_argument("Le véhiculedoit être en panne or: " + this->getEtat());        
+        throw runtime_error("Le véhicule doit être en panne or: " + this->getEtat());
     }
 
-    // Etat vehicule => ARRET     
+    // Etat vehicule => ARRET
     this->etat_ = this->ARRET;
-
 };
 
-void Vehicule::accelerer(int increment){
-
-    // au moins 1 occupant
-    if (this->occupants_ <= 0 ) {
-        throw new invalid_argument("Le véhicule doit avoir au moins un occupant.");
+void Vehicule::accelerer(int increment)
+{
+    if (increment <= 0)
+    {
+        throw invalid_argument("L'incrément d'accelération doit être strictement positive");
     }
 
-    // Vehicule en etat MARCHE 
-    if(this->etat_ != this->MARCHE) 
+    // au moins 1 occupant
+    if (this->occupants_ <= 0)
     {
-        throw new invalid_argument("Le véhicule est dans un mauvais état: " + this->getEtat());        
+        throw runtime_error("Le véhicule doit avoir au moins un occupant.");
+    }
+
+    // Vehicule en etat MARCHE
+    if (this->etat_ != this->MARCHE)
+    {
+        throw runtime_error("Le véhicule est dans un mauvais état: " + this->getEtat());
     }
 
     // La vitesse ne depasse pas la limite max
-    if ((this->vitesse_ +  increment) > this->vitesseMax_){
-        throw new invalid_argument("Le véhicule depasse la vitesse max de " + this->vitesseMax_);
+    if ((this->vitesse_ + increment) > this->vitesseMax_)
+    {
+        throw runtime_error("Le véhicule depasse la vitesse max de " + this->vitesseMax_);
     }
 
     // Incrementation de la vitesse
@@ -109,14 +121,22 @@ void Vehicule::accelerer(int increment){
 
 void Vehicule::monter(int nbOcc)
 {
-    //Il n'y a pas de place dispo dans la voiture 
-    if ((this->occupants_ +  nbOcc) > this->nbPlaces_){
-        throw new invalid_argument("Il n'y a plus de place dans le véhicule ");
+
+    if (nbOcc <= 0)
+    {
+        throw invalid_argument("Le nombre de personnes qui montent doit être strictement positif");
+    }
+
+    // Il n'y a pas de place dispo dans la voiture
+    if ((this->occupants_ + nbOcc) > this->nbPlaces_)
+    {
+        throw runtime_error("Il n'y a plus de place dans le véhicule ");
     }
 
     // Le vehicule est à l'arret
-    if (this->etat_ != this->ARRET){
-         throw new invalid_argument("Le véhicule est dans un mauvais état: " + this->getEtat());  
+    if (this->etat_ != this->ARRET)
+    {
+        throw runtime_error("Le véhicule est dans un mauvais état: " + this->getEtat());
     }
 
     this->occupants_ += nbOcc;
@@ -124,59 +144,83 @@ void Vehicule::monter(int nbOcc)
 
 void Vehicule::descendre(int nbOcc)
 {
-    //Il y a trop d'occupant dans la voiture 
-    if ((this->occupants_ - nbOcc) < this->nbPlaces_){
-        throw new invalid_argument("Il n'y a trop de monde dans le véhicule ");
+    if (nbOcc <= 0)
+    {
+        throw invalid_argument("Le nombre de personnes qui descendent doit être strictement positif");
+    }
+
+    // Il y a trop d'occupant dans la voiture
+    if ((this->occupants_ - nbOcc) < this->nbPlaces_)
+    {
+        throw runtime_error("Il n'y a trop de monde dans le véhicule ");
     }
 
     // Le vehicule est à l'arret
-    if (this->etat_ == this->MARCHE){
-         throw new invalid_argument("Le véhicule est dans un mauvais état: " + this->getEtat());  
+    if (this->etat_ == this->MARCHE)
+    {
+        throw runtime_error("Le véhicule est dans un mauvais état: " + this->getEtat());
     }
 
     this->occupants_ -= nbOcc;
 }
 
-void Vehicule::mettreEnPanne(double random){
-
-    //Le vehicule est déjà en panne
-    if(this->etat_ == this->PANNE_LEGERE || this->etat_ == this->PANNE_SEVERE) {
-        throw new invalid_argument("Le véhicule est dans un mauvais état: " + this->getEtat());  
-    }
+void Vehicule::mettreEnPanne(double random)
+{
 
     // Si random n'est pas compris entre 0 et 1
-    if (random > 1) {
-        throw new invalid_argument(" la valeure de random depasse 1");
+    if (random > 1)
+    {
+        throw invalid_argument("La valeur du nombre aléatoire depasse 1");
     }
-    if (random < 0) {
-        throw new invalid_argument(" la valeure de random est inférieure à 0");
+    if (random < 0)
+    {
+        throw invalid_argument("La valeur du nombre aléatoire est inférieure à 0");
     }
 
+    // Le vehicule est déjà en panne
+    if (this->etat_ == this->PANNE_LEGERE || this->etat_ == this->PANNE_SEVERE)
+    {
+        throw runtime_error("Le véhicule est dans un mauvais état: " + this->getEtat());
+    }
 
     // Si random est inférieur à 0.5 la panne sera légère, sinon elle sera sévère
-    if(random < 0,5) { 
+    if (random < 0.5)
+    {
         this->etat_ = this->PANNE_LEGERE;
     }
-    
-    if (random >= 0,5)
+
+    if (random >= 0.5)
     {
-        this->etat_ = this->PANNE_SEVERE;   
+        this->etat_ = this->PANNE_SEVERE;
     }
 
     this->vitesse_ = 0;
-
 }
 
-string Vehicule::getEtat() const {
+string Vehicule::getEtat() const
+{
     string res = "";
-    if(this->etat_ == this->MARCHE) { res = "MARCHE";}
-    else if(this->etat_ == this->PANNE_LEGERE) { res = "PANNE_LEGERE";}
-    else if(this->etat_ == this->PANNE_SEVERE) { res = "PANNE_SEVERE";}
-    else if(this->etat_ == this->ARRET) { res = "ARRET";}
-    
+    if (this->etat_ == this->MARCHE)
+    {
+        res = "MARCHE";
+    }
+    else if (this->etat_ == this->PANNE_LEGERE)
+    {
+        res = "PANNE_LEGERE";
+    }
+    else if (this->etat_ == this->PANNE_SEVERE)
+    {
+        res = "PANNE_SEVERE";
+    }
+    else if (this->etat_ == this->ARRET)
+    {
+        res = "ARRET";
+    }
+
     return res;
 }
 
-Vehicule::~Vehicule(){
+Vehicule::~Vehicule()
+{
     // destructeur
 }
